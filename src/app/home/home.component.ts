f58442ff15cc1320120be3e1b9334b0476b079d3
Jpeterson2012 <zerogravity@gmail.com>
2025-03-11 18:41:04 -0400

    new

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,30 +16,53 @@ export class HomeComponent implements OnInit{
   books: any = [];
   books2: any = []
   popbooks: any = [];
+  bookDes = ''
+  bookLink = ''
+  bookLink2 = ''
   ngOnInit(): void {
     
     this.db.getBooks(0,40).subscribe(
       v => {
         this.books = v.slice(0,20);
+        this.books = [...this.books,...this.books]        
         this.books2 = v.slice(20,40)
+        this.books2 = [...this.books2,...this.books2]
         // console.log(v)
         // console.log(this.books)
         // console.log(this.books2)
       }
     )    
-  }
-
-  images = [
-    {name: 'Everything is F*cked',image: '/assets/popbooks/mmevery.jpg'},
-    {name: 'One Good Deed',image: '/assets/popbooks/oneGoodDeed.jpg'},
-    {name: 'The Subtle Art of Not Giving a F*ck',image: '/assets/popbooks/subtleart.jpg'},
-    {name: 'In Cold Blood',image: '/assets/popbooks/tcInColdBlood.jpg'},
-    {name: 'The Cold Millions',image: '/assets/popbooks/theCold.jpg'}
-  ]
+  } 
 
   @ContentChild('longContent') longContent?: ElementRef
   openScrollableContent(longContent: any) {
 		this.modalService.open(longContent, { scrollable: true });
+	}
+
+  @ContentChild('longContent') longContent2?: ElementRef
+  async openScrollableContent2(longContent: any, isbn: any) {
+		this.modalService.open(longContent, { scrollable: true });             
+
+    let bookMetaData = await this.db.bookDesc(isbn)
+    let temp: any
+
+    bookMetaData.subscribe(v => {temp = v})        
+    this.bookLink2 = temp.openLibrary.key === undefined ? '' : "https://openlibrary.org/" + temp.openLibrary.key
+    this.bookLink = temp.google.totalItems === 0 ? '' : temp.google.items[0]!.volumeInfo.canonicalVolumeLink
+    this.bookDes = temp.google.totalItems === 0 ? 'Sorry, No Description Found' :temp.google.items[0]!.volumeInfo.description
+	}
+
+  @ContentChild('longContent') longContent3?: ElementRef
+  async openScrollableContent3(longContent: any, isbn: any) {
+		this.modalService.open(longContent, { scrollable: true });          
+
+    let bookMetaData = await this.db.bookDesc(isbn)
+    let temp: any
+
+    bookMetaData.subscribe(v => {temp = v})        
+    this.bookLink2 = temp.openLibrary.key === undefined ? '' : "https://openlibrary.org/" + temp.openLibrary.key
+    this.bookLink = temp.google.totalItems === 0 ? '' : temp.google.items[0]!.volumeInfo.canonicalVolumeLink
+    this.bookDes = temp.google.totalItems === 0 ? 'Sorry, No Description Found' :temp.google.items[0]!.volumeInfo.description
 	}
 
 
