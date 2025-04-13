426666efdf8c7cf6452869cf8a364adc8a8dae8a
Jpeterson2012 <zerogravity@gmail.com>
2025-04-13 14:56:15 -0400

    .

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,9 @@
 export const environment = {
     production: false,
-    apiAUTHURL: 'http://localhost:8080/api/auth/',
-    apiURL: 'http://localhost:8080/',
-    homeURL: 'http://localhost:4200/home'
+    apiAUTHURL: 'http://playground2.jmpeterson.dev/api/auth/',
+    apiURL: 'playground2.jmpeterson.dev/',
+    homeURL: 'http://bookshop.jmpeterson.dev/home'
+    // apiAUTHURL: 'http://localhost:8080/api/auth/',
+    // apiURL: 'http://localhost:8080/',
+    // homeURL: 'http://localhost:4200/home'
 };
