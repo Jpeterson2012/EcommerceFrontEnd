2545659836174664b408712405c64874b424b2e1
Jpeterson2012 <zerogravity@gmail.com>
2025-04-19 01:06:32 -0400

    .

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: false,
-    // apiAUTHURL: 'https://playground2.jmpeterson.dev/api/auth/',
-    // apiURL: 'https://playground2.jmpeterson.dev/',
-    // homeURL: 'https://bookshop.jmpeterson.dev/home'
-    apiAUTHURL: 'http://localhost:8080/api/auth/',
-    apiURL: 'http://localhost:8080/',
-    homeURL: 'http://localhost:4200/home'
+    apiAUTHURL: 'https://playground2.jmpeterson.dev/api/auth/',
+    apiURL: 'https://playground2.jmpeterson.dev/',
+    homeURL: 'https://bookshop.jmpeterson.dev/home'
+    // apiAUTHURL: 'http://localhost:8080/api/auth/',
+    // apiURL: 'http://localhost:8080/',
+    // homeURL: 'http://localhost:4200/home'
 };
